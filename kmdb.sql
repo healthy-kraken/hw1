31dc87118158822907897cf9844a2e61a5544c86
healthy-kraken <simon.serna@kellogg.northwestern.edu>
2026-04-18 18:38:26 +0000

    Final Product

--- kmdb.sql
+++ kmdb.sql
@@ -29,6 +29,82 @@
 -- * Note: The "guest" user role represents the experience prior to logging-in
 --   to an app and typically does not have a corresponding database table.
 
+
+
+
+-- Deliverables
+-- 
+-- There are five deliverables for this assignment, all delivered as part of
+-- this repository and submitted via GitHub and Canvas:
+--
+-- - An ERD (entity relationship diagram) illustrating the entities and
+--   relationships in this data model.
+-- - Implementation of the ERD via CREATE TABLE statements for each
+--   model/table. Include DROP TABLE IF EXISTS statements for each
+--   table, so that each run of this script starts with a blank database.
+-- - Insertion of initial sample data into tables.
+-- - Selection of data, so that something similar to the sample "report"
+--   below can be achieved.
+
+
+-- Rubric
+--
+-- 1. Domain model / ERD - 6 points
+-- - Draw an ERD (entity relationship diagram) with and add it as a file
+--   named "erd" (any image file extension type is acceptable).
+-- - Think about how the domain model needs to reflect the
+--   "real world" entities and the relationships with each other. 
+--   Hint #1: It's not just a single table that contains everything in the 
+--   expected output. There are multiple real world entities and
+--   relationships including at least one many-to-many relationship.
+--   Hint #2: Do NOT name one of your models/tables “cast” or “casts”; this 
+--   is a reserved word in sqlite and will break your database! Instead, 
+--   think of a better word to describe this concept; i.e. the relationship 
+--   between an actor and the movie in which they act.
+-- 2. Execution of the domain model (CREATE TABLE) - 4 points
+-- - Follow best practices for table and column names
+-- - Use correct data column types (i.e. TEXT/INTEGER)
+-- - Use of the `model_id` naming convention for foreign key columns
+-- 3. Insertion of data (INSERT statements) - 4 points
+-- - Insert data into all the tables you've created.
+-- 4. Modification of existing data (UPDATE statements) - 2 points
+-- - Assign an agent as the representative of a single actor.
+-- 5. "The report" (SELECT statements) - 4 points
+-- - Write 3 `SELECT` statements to produce (a) list of movies with their studio,
+--   (b) list of cast for each movie, and (c) list of actors the "agent" (i.e. user)
+--   is assigned to represent. See the "TODO" sections below and the sample output.
+--   You will need to read data from multiple tables in each `SELECT` statement.
+--   Formatting does not matter.
+-- 6. Using external resources for help with the assignment (including colleagues, AI, internet search, etc):
+-- - Engineers look to colleagues and the internet all the time when building software.
+--   You are welcome to do the same. However, the solution you submit must utilize
+--   the skills and strategies covered in class. Alternate solutions which do not demonstrate
+--   an understanding of the approaches used in class will receive significant deductions.
+--   Any concern should be raised with faculty prior to assignment due date.
+
+
+
+-- Submission
+-- 
+-- - "Use this template" to create a brand-new "hw1" repository in your
+--   personal GitHub account, e.g. https://github.com/<USERNAME>/hw1
+-- - Do the assignment, committing and syncing often
+-- - When done, commit and sync a final time, before submitting the GitHub
+--   URL for the finished "hw1" repository as the "Website URL" for the 
+--   Homework 1 assignment in Canvas
+
+-- Successful sample output is as shown:
+
+-- Turns column mode on but headers off
+-- .mode column
+-- .headers off
+
+-- Drop existing tables, so you'll start fresh each time this script is run.
+-- TODO!
+
+-- Create new tables, according to your domain model
+-- TODO!
+
 /*
 SPRINT 0: Creating ERD File and image upload
 */
@@ -40,9 +116,6 @@ SPRINT 0: Creating ERD File and image upload
 /*
 Sprint 1 - Table Creation for Requirement/Assumptions & User Stories
 */
-
-
-
 DROP TABLE IF EXISTS movies;
 DROP TABLE IF EXISTS studios;
 DROP TABLE IF EXISTS roster;
@@ -91,46 +164,12 @@ CREATE TABLE agents (
     last_name TEXT
     
 );
-
 /*
 */
 
-
--- Deliverables
--- 
--- There are five deliverables for this assignment, all delivered as part of
--- this repository and submitted via GitHub and Canvas:
---
--- - An ERD (entity relationship diagram) illustrating the entities and
---   relationships in this data model.
--- - Implementation of the ERD via CREATE TABLE statements for each
---   model/table. Include DROP TABLE IF EXISTS statements for each
---   table, so that each run of this script starts with a blank database.
--- - Insertion of initial sample data into tables.
--- - Selection of data, so that something similar to the sample "report"
---   below can be achieved.
-
-
--- Rubric
---
--- 1. Domain model / ERD - 6 points
--- - Draw an ERD (entity relationship diagram) with and add it as a file
---   named "erd" (any image file extension type is acceptable).
--- - Think about how the domain model needs to reflect the
---   "real world" entities and the relationships with each other. 
---   Hint #1: It's not just a single table that contains everything in the 
---   expected output. There are multiple real world entities and
---   relationships including at least one many-to-many relationship.
---   Hint #2: Do NOT name one of your models/tables “cast” or “casts”; this 
---   is a reserved word in sqlite and will break your database! Instead, 
---   think of a better word to describe this concept; i.e. the relationship 
---   between an actor and the movie in which they act.
--- 2. Execution of the domain model (CREATE TABLE) - 4 points
--- - Follow best practices for table and column names
--- - Use correct data column types (i.e. TEXT/INTEGER)
--- - Use of the `model_id` naming convention for foreign key columns
--- 3. Insertion of data (INSERT statements) - 4 points
--- - Insert data into all the tables you've created.
+-- Insert data into your database that reflects the sample data shown above
+-- Use hard-coded foreign key IDs when necessary
+-- TODO!
 
 /*
 SPRINT 2: Populating Tables
@@ -213,65 +252,21 @@ VALUES
 
 /**/
 
--- 4. Modification of existing data (UPDATE statements) - 2 points
--- - Assign an agent as the representative of a single actor.
--- 5. "The report" (SELECT statements) - 4 points
--- - Write 3 `SELECT` statements to produce (a) list of movies with their studio,
---   (b) list of cast for each movie, and (c) list of actors the "agent" (i.e. user)
---   is assigned to represent. See the "TODO" sections below and the sample output.
---   You will need to read data from multiple tables in each `SELECT` statement.
---   Formatting does not matter.
--- 6. Using external resources for help with the assignment (including colleagues, AI, internet search, etc):
--- - Engineers look to colleagues and the internet all the time when building software.
---   You are welcome to do the same. However, the solution you submit must utilize
---   the skills and strategies covered in class. Alternate solutions which do not demonstrate
---   an understanding of the approaches used in class will receive significant deductions.
---   Any concern should be raised with faculty prior to assignment due date.
-
-
-
--- Submission
--- 
--- - "Use this template" to create a brand-new "hw1" repository in your
---   personal GitHub account, e.g. https://github.com/<USERNAME>/hw1
--- - Do the assignment, committing and syncing often
--- - When done, commit and sync a final time, before submitting the GitHub
---   URL for the finished "hw1" repository as the "Website URL" for the 
---   Homework 1 assignment in Canvas
-
--- Successful sample output is as shown:
-
--- Turns column mode on but headers off
--- .mode column
--- .headers off
-
--- Drop existing tables, so you'll start fresh each time this script is run.
--- TODO!
-
--- Create new tables, according to your domain model
--- TODO!
-
--- Insert data into your database that reflects the sample data shown above
--- Use hard-coded foreign key IDs when necessary
--- TODO!
-
 -- Prints a header for the movies output
 .print ""
 .print "Movies"
 .print "====="
 .print ""
 
-/*
-SPRINT 3: Reading Table
-*/
+-- ***TODO!***
+-- The SQL statement for the movies output goes here.
 
---- Reading List of Movies with their studio
+/*
+SPRINT 3: Reading Table -- Reading List of Movies with their studio
+*/
 SELECT movies.title, movies.year_released, movies.mpaa_rating, studios.name FROM movies
 INNER JOIN studios ON movies.studio_id = studios.id;
 /**/
-
--- ***TODO!***
--- The SQL statement for the movies output goes here.
 
 -- Example output:
 -- Movies
@@ -286,8 +281,17 @@ INNER JOIN studios ON movies.studio_id = studios.id;
 .print "====="
 .print ""
 
+
 -- ***TODO!***
 -- The SQL statement for the cast output goes here.
+/*
+SPRINT 4: Reading Table -- Reading Casting List
+*/ 
+
+SELECT movies.title, actors.first_name, actors.last_name, roster.character_name FROM movies
+INNER JOIN roster ON roster.movie_id = movies.id 
+INNER JOIN actors ON actors.id = roster.actor_id;
+/**/
 
 -- Example output:
 -- Top Cast
@@ -314,8 +318,20 @@ INNER JOIN studios ON movies.studio_id = studios.id;
 .print "====="
 .print ""
 
+
+
 -- ***TODO!***
 -- The SQL statement for the represented actor(s) output goes here.
+
+/*
+SPRINT 5: Reading Table -- Actors represented
+*/
+
+SELECT actors.first_name, actors.last_name, agents.first_name, agents.last_name FROM actors
+INNER JOIN networks ON networks.actor_id = actors.id
+INNER JOIN agents ON agents.id = networks.agent_id;
+/**/
+
 
 -- Example output:
 -- Represented by agent
